fa7bead72c00474b9d5344e46889854da726700c
fangkai <1209683835@qq.com>
2022-04-22 10:55:29 +0800

    feat: http-exception

--- core/http-exception.js
+++ core/http-exception.js
@@ -1,54 +1,54 @@
 class HttpException extends Error {
-  constructor(msg = '服务器异常', errorCode = 10000, code = 400) {
+  constructor(code = 400, errorCode = 10000, msg = '服务器异常') {
     super()
-    this.errorCode = errorCode
     this.code = code
+    this.errorCode = errorCode
+    this.msg = msg
+  }
+}
+
+class Success extends HttpException {
+  constructor(errorCode = 0, msg = 'ok') {
+    super()
+    this.code = 201
+    this.errorCode = errorCode
     this.msg = msg
   }
 }
 
 class ParameterException extends HttpException {
-  constructor(msg = '参数错误', errorCode = 10001) {
+  constructor(errorCode = 10001, msg = '参数错误') {
     super()
-    this.errorCode = errorCode
-    this.msg = msg
     this.code = 400
-  }
-}
-
-class Success extends HttpException {
-  constructor(msg = 'ok', errorCode = 0) {
-    super()
     this.errorCode = errorCode
     this.msg = msg
-    this.code = 201
-  }
-}
-
-class NotFound extends HttpException {
-  constructor(msg = '资源未找到', errorCode = 10000) {
-    super()
-    this.errorCode = errorCode
-    this.msg = msg
-    this.code = 404
   }
 }
 
 class AuthFailed extends HttpException {
-  constructor(msg = '授权失败', errorCode = 10004) {
+  constructor(errorCode = 10004, msg = '授权失败') {
     super()
+    this.code = 401
     this.errorCode = errorCode
     this.msg = msg
-    this.code = 401
   }
 }
 
 class Forbbiden extends HttpException {
-  constructor(msg = '禁止访问', errorCode = 10006) {
+  constructor(errorCode = 10006, msg = '禁止访问') {
     super()
+    this.code = 403
     this.errorCode = errorCode
     this.msg = msg
-    this.code = 403
+  }
+}
+
+class NotFound extends HttpException {
+  constructor(errorCode = 10000, msg = '资源未找到') {
+    super()
+    this.code = 404
+    this.errorCode = errorCode
+    this.msg = msg
   }
 }
 
