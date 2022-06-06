2de8172b6fa217c3ea61ceb0b9e0401dc4770e8d
fangkai <1209683835@qq.com>
2022-06-06 14:31:53 +0800

    feat: 用户表

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,40 @@
-const { Model } = require("sequelize");
+const { Sequelize, Model } = require("sequelize");
+const { sequelize } = require("@core/db")
 
 class User extends Model {
 
 }
+
+User.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nickname: Sequelize.STRING,
+    email: {
+      type: Sequelize.STRING(128),
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      set(val) {
+        const salt = bcrypt.genSaltSync(10)
+        const pwd = bcrypt.hashSync(val, salt)
+        this.setDataValue('password', pwd)
+      }
+    },
+    openid: {
+      type: Sequelize.STRING(64),
+      unique: true
+    }
+  },
+  {
+    sequelize,
+    tableName: 'user'
+  }
+)
 
 module.exports = {
   User
