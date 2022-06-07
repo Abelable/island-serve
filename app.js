1f2f239f83c1ec8ad98b1f503fa8e91f8ead6aee
fangkai <1209683835@qq.com>
2022-06-07 17:08:09 +0800

    feat: 图片处理

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ require('module-alias/register')
 
 const Koa = require("koa")
 const bodyparser = require("koa-bodyparser")
+const path = require('path')
+const static = require('koa-static')
 const catchError = require('./middlewares/exception')
 const InitManager = require('./core/init')
 
 const app = new Koa()
-app.use(catchError)
 app.use(bodyparser())
+app.use(static(path.join(__dirname, './static')))
+app.use(catchError)
 InitManager.initCore(app)
 
 app.listen(3001)
