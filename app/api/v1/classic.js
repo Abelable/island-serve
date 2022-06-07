bac8d1674b305c73a5551d1bd57cf5ffe724139c
fangkai <1209683835@qq.com>
2022-06-07 18:39:38 +0800

    feat: 前后期刊

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -12,33 +12,29 @@ const router = new Router({
 
 router.get("/latest", new Auth().middlewave, async (ctx) => {
   const flow = await Flow.findOne({ order: [["index", "DESC"]] })
-  const favor = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid)
-  const art = await Art.getData(flow.art_id, flow.type)
-  art.setDataValue('index', flow.index)
-  art.setDataValue('like_status', favor)
-  ctx.body = art
+  ctx.body = await getArt(flow, ctx.auth.uid)
 })
 
 router.get("/:index/next", new Auth().middlewave, async (ctx) => {
   const v = await new PositiveIntegerValidator().validate(ctx, { id: "index" })
   const flow = await Flow.findOne({ where: { index: parseInt(v.get("path.index")) + 1 } })
   if (!flow) throw new NotFound("没有找到下一期期刊")
-  const favor = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid)
-  const art = await Art.getData(flow.art_id, flow.type)
-  art.setDataValue('index', flow.index)
-  art.setDataValue('like_status', favor)
-  ctx.body = art
+  ctx.body = await getArt(flow, ctx.auth.uid)
 })
 
 router.get("/:index/previous", new Auth().middlewave, async (ctx) => {
   const v = await new PositiveIntegerValidator().validate(ctx, { id: "index" })
   const flow = await Flow.findOne({ where: { index: v.get("path.index") - 1 } })
   if (!flow) throw new NotFound("没有找到上一期期刊")
-  const favor = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid)
+  ctx.body = await getArt(flow, ctx.auth.uid)
+})
+
+const getArt = async (flow, uid) => {
+  const favor = await Favor.userLikeIt(flow.art_id, flow.type, uid)
   const art = await Art.getData(flow.art_id, flow.type)
   art.setDataValue('index', flow.index)
   art.setDataValue('like_status', favor)
-  ctx.body = art
-})
+  return art
+}
 
 module.exports = router
