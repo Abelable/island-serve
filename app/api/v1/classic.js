fca8d0e263d4ef9e492f3cad8be0752710597ba3
fangkai <1209683835@qq.com>
2022-06-07 16:46:54 +0800

    feat: classic接口

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -2,6 +2,7 @@ const Router = require("koa-router")
 const { Auth } = require("@root/middlewares/auth")
 const { Flow } = require("@root/app/models/flow")
 const { Art } = require("@root/app/models/art")
+const { Favor } = require("@root/app/models/favor")
 
 const router = new Router({
   prefix: "/v1/classic"
@@ -9,8 +10,10 @@ const router = new Router({
 
 router.get("/latest", new Auth().middlewave, async (ctx) => {
   const flow = await Flow.findOne({ order: [["index", "DESC"]] })
+  const favor = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid)
   const art = await Art.getData(flow.art_id, flow.type)
   art.setDataValue('index', flow.index)
+  art.setDataValue('like_status', favor)
   ctx.body = art
 })
 
