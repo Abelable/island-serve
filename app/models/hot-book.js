0adcf82536d661344af52ebdaf874062d9306d82
fangkai <1209683835@qq.com>
2022-06-08 11:22:30 +0800

    feat: 书籍接口

---
+++ app/models/hot-book.js
@@ -0,0 +1,42 @@
+const { Model, Sequelize, Op } = require("sequelize");
+const { sequelize } = require("@core/db");
+const { Favor } = require("./favor");
+
+class HotBook extends Model {
+  static async getAll() {
+    const books = await HotBook.getAll({ order: ["index"] })
+    const ids = books.map(book => book.id)
+    const favors = await Favor.findAll({
+      where: {
+        art_id: {
+          [Op.in]: ids,
+          type: 400
+        }
+      },
+      group: ["art_id"],
+      attributes: ["art_id", [Sequelize.fn("COUNT", "*"), "count"]] // ?: 笛卡尔积
+    })
+    books.forEach(book => HotBook._getEachBookStatus(book, favors))
+    return books
+  }
+
+  static _getEachBookStatus(book, favors) {
+    const count = favors.find(favor => favor.art_id === book.id).get("count")
+    book.setDataValue("fav_nums", count)
+    return book
+  }
+}
+
+HotBook.init({
+  index: Sequelize.INTEGAR,
+  image: Sequelize.STRING,
+  author: Sequelize.STRING,
+  title: Sequelize.STRING,
+}, {
+  sequelize,
+  tableName: "hot_book"
+})
+
+module.exports = {
+  HotBook
+}
